f6491656b07706e69ebffd03c1dc063a9bfa2813
anna <anna@192.168.1.110>
2017-05-20 22:55:14 +0800

    UPDATE MODEL NAME

---
+++ himalaya-search/src/main/java/com/himalaya/queryparse/MyQueryParse.java
@@ -0,0 +1,11 @@
+package com.himalaya.queryparse;
+/**
+* @author: xuqu
+* @E-mail: fredxuqu@163.com
+* @version 
+* 2017年5月24日 上午9:29:33
+* Description
+*/
+public class MyQueryParse {
+	
+}
---
+++ himalaya-search/src/main/java/com/himalaya/similarity/MySolrSimilarity.java
@@ -0,0 +1,11 @@
+package com.himalaya.similarity;
+/**
+* @author: xuqu
+* @E-mail: fredxuqu@163.com
+* @version 
+* 2017年5月23日 下午5:20:34
+* Description
+*/
+public class MySolrSimilarity extends DefaultSimilarity{
+
+}
