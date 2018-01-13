59ca6542c940e2fcdd0031bfdb3439161ab180cf
fredxuqu <fredxuqu@163.com>
2018-01-13 16:55:32 +0800

    UPDATE SQL INITIALIZATION SCRIPTS

--- himalaya-corejava/src/main/java/com/himalaya/compiletype/CompileTypeDemo.java
+++ himalaya-corejava/src/main/java/com/himalaya/compiletype/CompileTypeDemo.java
@@ -1,50 +1,50 @@
-package com.himalaya.compiletype;
-
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.CtConstructor;
-import javassist.CtField;
-import javassist.CtMethod;
-
-/**
-* @author: xuqu
-* @E-mail: fredxuqu@163.com
-* @version 
-* 2017年6月23日 下午1:23:46
-* Java字节码技术
-* 采用JavaAssist实现
-*/
-public class CompileTypeDemo {
-
-	public static void main(String[] args) throws Exception {
-		
-		ClassPool pool = ClassPool.getDefault();
-		
-		CtClass cc = pool.makeClass("com.himalaya.compiletype.autogen.Entity");
-	
-		// create field
-		CtField fname = CtField.make("private String name;", cc);
-		CtField fid = CtField.make("private int id;", cc);
-		cc.addField(fname);
-		cc.addField(fid);
-		// create method
-		CtMethod mSetName = CtMethod.make("public void setName(String name){$0.name = $1;}", cc);
-		CtMethod mSetId = CtMethod.make("public void setId(String id){$0.id = $1;}", cc);
-		CtMethod mGetName = CtMethod.make("public String getName(){return $0.name;}", cc);
-		CtMethod mGetId = CtMethod.make("public int getId(){return $0.id;}", cc);
-		cc.addMethod(mSetName);
-		cc.addMethod(mSetId);
-		cc.addMethod(mGetName);
-		cc.addMethod(mGetId);
-		
-		// add constructor
-		CtConstructor constructor = new CtConstructor(new CtClass[]{CtClass.intType,pool.get("java.lang.String")}, cc);
-		constructor.setBody("{$0.id = $1;$0.name = $2;}");
-		cc.addConstructor(constructor);
-		
-		String path = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-		System.out.println(path);
-		cc.writeFile(path);
-		System.out.println("Class generated successful");
-	}
-}
+//package com.himalaya.compiletype;
+//
+//import javassist.ClassPool;
+//import javassist.CtClass;
+//import javassist.CtConstructor;
+//import javassist.CtField;
+//import javassist.CtMethod;
+//
+///**
+//* @author: xuqu
+//* @E-mail: fredxuqu@163.com
+//* @version 
+//* 2017年6月23日 下午1:23:46
+//* Java字节码技术
+//* 采用JavaAssist实现
+//*/
+//public class CompileTypeDemo {
+//
+//	public static void main(String[] args) throws Exception {
+//		
+//		ClassPool pool = ClassPool.getDefault();
+//		
+//		CtClass cc = pool.makeClass("com.himalaya.compiletype.autogen.Entity");
+//	
+//		// create field
+//		CtField fname = CtField.make("private String name;", cc);
+//		CtField fid = CtField.make("private int id;", cc);
+//		cc.addField(fname);
+//		cc.addField(fid);
+//		// create method
+//		CtMethod mSetName = CtMethod.make("public void setName(String name){$0.name = $1;}", cc);
+//		CtMethod mSetId = CtMethod.make("public void setId(String id){$0.id = $1;}", cc);
+//		CtMethod mGetName = CtMethod.make("public String getName(){return $0.name;}", cc);
+//		CtMethod mGetId = CtMethod.make("public int getId(){return $0.id;}", cc);
+//		cc.addMethod(mSetName);
+//		cc.addMethod(mSetId);
+//		cc.addMethod(mGetName);
+//		cc.addMethod(mGetId);
+//		
+//		// add constructor
+//		CtConstructor constructor = new CtConstructor(new CtClass[]{CtClass.intType,pool.get("java.lang.String")}, cc);
+//		constructor.setBody("{$0.id = $1;$0.name = $2;}");
+//		cc.addConstructor(constructor);
+//		
+//		String path = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+//		System.out.println(path);
+//		cc.writeFile(path);
+//		System.out.println("Class generated successful");
+//	}
+//}
--- himalaya-doc/documents/080 Architecture/Solr/init.sql
+++ himalaya-doc/documents/080 Architecture/Solr/init.sql
@@ -1,41 +1,21 @@
-DROP TABLE PRODUCTS;
-CREATE TABLE PRODUCTS(
-PRODUCT_ID INT PRIMARY KEY,
-PRODUCT_NAME VARCHAR2(100),
-MADEIN VARCHAR2(10),
-PRODUCT_VERSION VARCHAR2(20),
-PRODUCT_DESC VARCHAR2(1000),
-PRODUCT_TYPE VARCHAR2(2),
-MODIFY_TIME DATE
-);
+/*
+SQLyog Enterprise v12.09 (64 bit)
+MySQL - 5.7.17-log 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
 
-Insert into PRODUCTS (PRODUCT_ID,PRODUCT_NAME,MADEIN,PRODUCT_VERSION,PRODUCT_DESC,PRODUCT_TYPE,MODIFY_TIME) 
-values (1,'JAVA编程思想第四版','USA','1.0.0.113','JAVA编程思想第四版','1',SYSDATE);
-Insert into PRODUCTS (PRODUCT_ID,PRODUCT_NAME,MADEIN,PRODUCT_VERSION,PRODUCT_DESC,PRODUCT_TYPE,MODIFY_TIME) 
-values (2,'机器学习实践','USA','sdkf.2.009.12','机器学习实践 2 ','1',SYSDATE);
-Insert into PRODUCTS (PRODUCT_ID,PRODUCT_NAME,MADEIN,PRODUCT_VERSION,PRODUCT_DESC,PRODUCT_TYPE,MODIFY_TIME) 
-values (3,'比亚迪新能源车','CHINA','TANG','比亚迪新能源车','2',SYSDATE);
-Insert into PRODUCTS (PRODUCT_ID,PRODUCT_NAME,MADEIN,PRODUCT_VERSION,PRODUCT_DESC,PRODUCT_TYPE,MODIFY_TIME) 
-values (4,'Macbook Pro 13','USA','Macbook.0.113','苹果笔记本电脑，retina显示屏，13英寸','2',SYSDATE);
-Insert into PRODUCTS (PRODUCT_ID,PRODUCT_NAME,MADEIN,PRODUCT_VERSION,PRODUCT_DESC,PRODUCT_TYPE,MODIFY_TIME) 
-values (5,'Oracle企业开发指南','CHINA','4','最全的oracle开发指南','1',SYSDATE);
-Insert into PRODUCTS (PRODUCT_ID,PRODUCT_NAME,MADEIN,PRODUCT_VERSION,PRODUCT_DESC,PRODUCT_TYPE,MODIFY_TIME) 
-values (6,'LENOVO LAPTOP','CHINA','LS410','Lenovo, lowest price, best quality','2',SYSDATE);
-Insert into PRODUCTS (PRODUCT_ID,PRODUCT_NAME,MADEIN,PRODUCT_VERSION,PRODUCT_DESC,PRODUCT_TYPE,MODIFY_TIME) 
-values (7,'SUMSING','SKOREA','GALAXY S5','tips will help you connect with your customers.','2',SYSDATE);
-
-DROP TABLE PRODUCT_TYPE;
-CREATE TABLE PRODUCT_TYPE(
-TYPE_ID INT PRIMARY KEY,
-TYPE_NAME VARCHAR2(20),
-MODIFY_TIME DATE
-);
-INSERT INTO PRODUCT_TYPE(TYPE_ID, TYPE_NAME, MODIFY_TIME) VALUES (1, 'Books', SYSDATE);
-INSERT INTO PRODUCT_TYPE(TYPE_ID, TYPE_NAME, MODIFY_TIME) VALUES (2, 'Tools', SYSDATE);
-
-UPDATE PRODUCT_TYPE SET TYPE_NAME = 'Books1', MODIFY_TIME=SYSDATE WHERE TYPE_ID=1;
-
-UPDATE PRODUCTS SET PRODUCT_DESC = '机器学习实践 2 2 UPDATED', MODIFY_TIME=SYSDATE WHERE PRODUCT_ID = 2;
-
-Insert into PRODUCTS (PRODUCT_ID,PRODUCT_NAME,MADEIN,PRODUCT_VERSION,PRODUCT_DESC,PRODUCT_TYPE,MODIFY_TIME) 
-values (8,'Iphone6 Plus','USA','Iphone6 Plus','Iphone6 Plus.','2',SYSDATE);
+create table `TB_NEWS` (
+	`ID` int (10),
+	`TITLE` varchar (300),
+	`CONTENT` varchar (12000),
+	`AUTHER` varchar (300),
+	`TYPE` int (2),
+	`PUBLISH_TIME` datetime ,
+	`CREATE_TIME` datetime ,
+	`UPDATE_TIME` datetime 
+); 
+insert into `TB_NEWS` (`ID`, `TITLE`, `CONTENT`, `AUTHER`, `TYPE`, `PUBLISH_TIME`, `CREATE_TIME`, `UPDATE_TIME`) values('1','上将“军老虎”落马，他究竟向谁行贿？','本周，最重要的消息是十九届中央纪委二次全会11日在北京开幕，中共中央总书记、国家主席、中央军委主席习近平发出了新号召，“重整行装再出发，以永远在路上的执着把全面从严治党引向深入，开创全面从严治党新局面。”\n1月12日，广东省委原常委、珠海市委原书记李嘉受贿案公开宣判，李嘉受贿2058万余元，获刑13年，罚金200万元。他是十九大之后第一个获刑的原省部级官员。\n本周最令人震惊的消息莫过于一位重磅“军老虎”的倒下。房峰辉，这位中央军事委员会委员因涉嫌行贿、受贿犯罪，被移送军事检察机关依法处理。军中反腐，大刀阔斧。','幸福西饼','1','2018-01-13 16:22:46','2018-01-13 16:22:46','2018-01-13 16:22:46');
+insert into `TB_NEWS` (`ID`, `TITLE`, `CONTENT`, `AUTHER`, `TYPE`, `PUBLISH_TIME`, `CREATE_TIME`, `UPDATE_TIME`) values('2','“夜宿门”后李小璐首现身，与甜馨同行，专注手机，心情大好','李小璐夜宿pgone事件已经过去十多天。随着李小璐的“消失”，贾乃亮的声明，pgone回到老家，该事件的主要角色来了个大换身，成了马苏和黄毅清。而马苏起诉黄毅清，而且还是刑事自诉黄毅清，更是将两人的关系推上了风口浪尖。\n马苏与黄毅清的官司如火如荼，李小璐一家三口则悠然自得，心情大好。黄毅清不愧是贾乃亮的好兄弟，马苏也不愧是李小璐的好姐妹。挡枪眼，最终成为主角。','幸福西饼','2','2018-01-13 16:25:47','2018-01-13 16:25:47','2018-01-13 16:25:47');
+insert into `TB_NEWS` (`ID`, `TITLE`, `CONTENT`, `AUTHER`, `TYPE`, `PUBLISH_TIME`, `CREATE_TIME`, `UPDATE_TIME`) values('3','专心治疗不安排训练 邹市明的拳击生涯会就此终结吗','原标题：专心治疗不安排训练，邹市明的拳击生涯会就此终结吗\n“好久不见，我还是我……”昨日，备受关注的奥运冠军、前世界拳王邹市明通过其官方微博发声，透露自己已经离开医院。\n此时，距离他上月21日被曝出因眼疾入院已经过去了23天。\n澎湃新闻记者通过邹市明团队获悉，邹市明在接受了治疗后，病情相对稳定，接下来将在家中静养，但院方还将会诊评估邹市明的病情，并确定下一步治疗方案。\n简而言之，邹市明一段时间内依然会以治疗康复为主，不会有训练和比赛安排。而这是否会就此成为他职业生涯的终结，只能等待邹市明自己的决定。','凤凰新闻','3','2018-01-13 16:25:48','2018-01-13 16:25:48','2018-01-13 16:25:48');
+insert into `TB_NEWS` (`ID`, `TITLE`, `CONTENT`, `AUTHER`, `TYPE`, `PUBLISH_TIME`, `CREATE_TIME`, `UPDATE_TIME`) values('4','捷克选总统可能需要第二轮投票','（原标题：捷克选总统可能需要第二轮投票）\n新华社北京1月13日电（记者沈敏）捷克共和国1月12日至13日直接选举总统，选举可能需要第二轮投票。\n多项选前民意调查显示，9名候选人中，现任总统米洛什·泽曼所获支持率领先，有望在首轮投票中“出线”。\n但是，民意分裂以及民意支持率分散所致，泽曼如果拿不到50%选票，则需要进入第二轮，可能与捷克科学院前院长伊日·德拉霍什对决。','网易新闻','4','2018-01-13 16:25:48','2018-01-13 16:25:48','2018-01-13 16:25:48');
