5d41cd15485588f291182e8bec161b86e160f4a6
xuqu@pycredit.cn <Study1010>
2020-05-21 17:33:22 +0800

    tidy up

--- himalaya-rocketmq/src/main/java/com/himalaya/repository/impl/LogRepositoryImpl.java
+++ himalaya-rocketmq/src/main/java/com/himalaya/repository/impl/LogRepositoryImpl.java
@@ -3,9 +3,6 @@ package com.himalaya.repository.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
